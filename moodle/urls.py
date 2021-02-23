caf214e1706b87d78e1ccdec397b48670e022a18
lkk7 <lukaszlapinski7@gmail.com>
2021-02-23 17:52:46 +0100

    Add list view and detailed view for Lessons and Courses

--- moodle/urls.py
+++ moodle/urls.py
@@ -8,6 +8,12 @@ from . import views
 app_name = 'moodle'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lesson/list/', views.LessonListView.as_view(),
+         name='lesson_list'),
     path('lesson/<int:pk>/', views.LessonDetailView.as_view(),
-         name='lesson_view')
+         name='lesson_view'),
+    path('course/list/', views.CourseListView.as_view(),
+         name='lesson_list'),
+    path('course/<int:pk>/', views.CourseDetailView.as_view(),
+         name='course_view')
 ]
--- moodle/views.py
+++ moodle/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import HttpResponse
 from .models import Course, Lesson, Question
 from django.views import generic
+from django.db.models import Q
 
 
 def index(request):
@@ -8,6 +9,16 @@ def index(request):
         return HttpResponse('index.')
     else:
         return HttpResponse('index. log in.')
+
+
+class LessonListView(generic.ListView):
+    template_name = 'moodle/lesson_list.html'
+
+    def get_queryset(self):
+        return Lesson.objects.filter(
+            Q(course__students__id__contains=self.request.user.id)
+            | Q(course__teachers__id__contains=self.request.user.id)
+        ).distinct()
 
 
 class LessonDetailView(generic.DetailView):
@@ -18,4 +29,25 @@ class LessonDetailView(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['questions'] = Question.objects.filter(
             lesson=self.get_object())
+        return context
+
+
+class CourseListView(generic.ListView):
+    template_name = 'moodle/course_list.html'
+
+    def get_queryset(self):
+        return Course.objects.filter(
+            Q(students__id__contains=self.request.user.id)
+            | Q(teachers__id__contains=self.request.user.id)
+        ).distinct()
+
+
+class CourseDetailView(generic.DetailView):
+    model = Course
+    template_name = 'moodle/course_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lessons'] = Lesson.objects.filter(
+            course=self.get_object())
         return context
