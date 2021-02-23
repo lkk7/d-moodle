5b8cc5a710aa6d0872f0123edc7a2bd50340bed3
lkk7 <lukaszlapinski7@gmail.com>
2021-02-23 19:21:00 +0100

    Add registered students to the Student group

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import Group
 from .forms import SignUpForm
 
 
@@ -11,6 +12,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
+            Group.objects.get(name='Students').user_set.add(user)
             login(request, user)
             return redirect('moodle:index')
     else:
